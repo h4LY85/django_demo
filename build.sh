20ae6c41be59ed16d2dfbdc05372d44710fcc175
h4LY85 <45350097+h4LY85@users.noreply.github.com>
2025-03-10 15:17:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
-echo "hello start"
-
-poetry env remove --all
-
-poetry update
-poetry install --no-dev --no-interaction
+poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
