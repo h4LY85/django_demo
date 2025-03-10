995066c77b215ab8235207316fcb223f73e2aa6c
h4LY85 <45350097+h4LY85@users.noreply.github.com>
2025-03-10 14:54:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-pip install --upgrade poetry
-poetry env remove --all
-poetry add "rembg@^2.0.64" "onnxruntime@^1.16.3"
+
 poetry install --no-dev --no-interaction
 
 python manage.py collectstatic --no-input
